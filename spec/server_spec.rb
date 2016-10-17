bc4f5d8b1e360bc6be72b9efc21029962dc73bb8
fabien townsend <fabientownsend@gmail.com>
2016-10-17 11:26:33 +0100

    rack server with basic test

---
+++ spec/server_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require 'rack/test'
+
+describe 'the app' do
+  include Rack::Test::Methods
+
+  def app
+    Rack::Builder.new do
+      map '/' do
+        run Proc.new {|env| [200, {'Content-Type' => 'text/html'}, 'foo'] }
+      end
+    end
+  end
+
+  it "say foo" do
+    get '/'
+    last_response.should be_ok
+    last_response.body.should == 'foo'
+  end
+end
