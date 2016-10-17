9a1ceb86bd9b37de0f8c528ebd8f88824481d915
fabien townsend <fabientownsend@gmail.com>
2016-10-17 16:39:29 +0100

    remove useless method

--- lib/server.rb
+++ lib/server.rb
@@ -1,17 +1,8 @@
 require "rack"
 
 class Server
-  RESPONSE = ['200', {'Content-Type' => 'text/html'}, ['hello']]
 
   def call(env)
-    RESPONSE
-  end
-
-  def run
-    app = Proc.new do |env|
-      RESPONSE
-    end
-
-    app
+    ['200', {'Content-Type' => 'text/html'}, ['hello']]
   end
 end
