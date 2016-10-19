ef94aeaba2a976213a1d7cf33d41434d51a38b2b
fabien townsend <fabientownsend@gmail.com>
2016-10-19 09:08:34 +0100

    add the class board from the gem

--- lib/server.rb
+++ lib/server.rb
@@ -1,15 +1,40 @@
 require "rack"
+require "board"
 require "cgi"
 
 class Server
+  def initialize(board)
+    @board = board
+  end
+
   def call(env)
-  board = "board"
+  board = @board.board
+
     if (env["PATH_INFO"] == "/move")
       values = CGI.parse(env["QUERY_STRING"])
-      puts values["number"]
-      board = "new board"
+      position = values["number"].first
+      if is_integer(position) && available?(Integer(position)) && in_range?(Integer(position))
+        position = Integer(position)
+        @board.set_mark(Mark::CROSS, position)
+      end
     end
 
-    ['200', {'Content-Type' => 'text/html'}, [board]]
+    ['200', {'Content-Type' => 'text/html'}, [board.to_s]]
+  end
+
+  def available?(position)
+    @board.free_positions.include?(position)
+  end
+
+  def in_range?(position)
+    position >= @board.POSITION_MIN && position < @board.POSITION_MAX
+  end
+
+  def is_integer(value)
+    begin
+      Integer(value)
+    rescue
+      return false
+    end
   end
 end
--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -2,26 +2,45 @@ require 'rspec'
 require 'rack/test'
 require 'spec_helper'
 require 'server'
+require 'board'
+require 'board_helper'
 
 RSpec.describe Server do
   include Rack::Test::Methods
 
   def app
-    Server.new
+    board = Board.new
+    @helper = BoardHelper.new(board)
+    Server.new(board)
   end
 
   it "display de default page" do
     get "/"
     expect(last_response).to be_ok
     expect(last_request.url).to eq("http://example.org/")
-    expect(last_response.body).to eq("board")
+    expect(@helper.board_to_string).to eq("   ,   ,   ")
   end
 
   it "recieve the value from the player" do
-    get "/move?number=asdfas"
+    get "/move?number=1"
     expect(last_response).to be_ok
-    expect(last_request.url).to eq("http://example.org/move?number=asdfas")
-    expect(last_response.body).to eq("new board")
+    expect(last_request.url).to eq("http://example.org/move?number=1")
     expect(last_request.env["PATH_INFO"]).to eq("/move")
+    expect(@helper.board_to_string).to eq(" X ,   ,   ")
+  end
+
+  it "doesn't set a string value" do
+    get "/move?number=asdfjl"
+    expect(@helper.board_to_string).to eq("   ,   ,   ")
+  end
+
+  it "doesn't set the value too high" do
+    get "/move?number=10"
+    expect(@helper.board_to_string).to eq("   ,   ,   ")
+  end
+
+  it "doesn't set the value too low" do
+    get "/move?number=-10"
+    expect(@helper.board_to_string).to eq("   ,   ,   ")
   end
 end
