9eee0e6151eef162bd2be3a920b901c2cef70937
fabien townsend <fabientownsend@gmail.com>
2016-10-19 16:01:18 +0100

    creation board html

--- lib/server.rb
+++ lib/server.rb
@@ -10,7 +10,7 @@ class Server
   end
 
   def call(env)
-    html = html_builder(@game.status, @board.board)
+    html = html_builder(@game.status, board_builder(@board.board))
 
     if (env["PATH_INFO"] == "/move")
       values = CGI.parse(env["QUERY_STRING"])
@@ -19,9 +19,9 @@ class Server
       @game.play(position)
 
       if !@game.over?
-        html = html_builder(@game.player_turn, @board.board)
+        html = html_builder(@game.player_turn, board_builder(@board.board))
       else
-        html = html_builder(@game.status, @board.board)
+        html = html_builder(@game.status, board_builder(@board.board))
       end
     end
 
@@ -34,5 +34,21 @@ class Server
     "<!doctype html><html lang=''><head><meta charset='utf-8'><title></title></head><body>
     <h1>Tic-Tac-Toe</h1><p>#{message}</p><div id='board'>#{board}</div><form action='move'>
     <input name='number' type='number' /><input type='submit' value='Submit'/></form></body></html>"
+  end
+
+  def board_builder(board)
+    html_board = "<table><tbody>"
+
+    board.each do |line|
+      html_board += "<tr>"
+
+      line.each do |spot|
+        html_board += "<td> #{spot} </td>"
+      end
+
+      html_board += "</tr>"
+    end
+
+    html_board += "</tbody></table>"
   end
 end
